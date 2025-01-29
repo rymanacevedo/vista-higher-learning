a4e7587a73bc8e75fef8507ba5511bf64e0196e2
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-29 03:17:46 +0000

    adjusted button styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-
-function App() {
+export default function App() {
   return (
     <>
-      <h1 className=''>Say the vocabulary words</h1>
-      <div className="flex eitems-center justify-items-center">
-        <button className='inline-flex justify-center rounded-md border border-transparent cursor-pointer bg-blue-600 text-white'>
-          <span className="square-symbol"></span>
+      <h1 className="flex justify-center items-center">Say the vocabulary words</h1>
+      <div className="space-x-4 flex items-center justify-center">
+        <button className="inline-flex items-center justify-center rounded-full border border-transparent cursor-pointer bg-blue-600 text-white p-4">
+          <span className="w-4 h-4 bg-white mr-2" />
           <span>Stop</span>
         </button>
-        <button className="inline-flex justify-center rounded-md border border-transparent cursor-pointer bg-red-600">
-          <span className="circle-symbol"></span>
+
+        <button className="inline-flex items-center justify-center rounded-full border border-transparent cursor-pointer bg-red-600 text-white p-4">
+          <span className="w-4 h-4 bg-white rounded-full mr-2" />
           <span>Record</span>
         </button>
 
-        <button className="inline-flex justify-center rounded-md border border-transparent cursor-pointer bg-green-800">
-          <span className="triangle-symbol"></span>
+        <button className="inline-flex items-center justify-center rounded-full border border-transparent cursor-pointer bg-green-800 text-white p-4">
+          <span className="w-0 h-0 border-t-[8px] border-t-transparent border-b-[8px] border-b-transparent border-l-[12px] border-l-white mr-2" />
           <span>Review your recording</span>
         </button>
       </div>
     </>
-  )
+  );
 }
-
-export default App;
