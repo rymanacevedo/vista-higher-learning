57cb84fcfbad9b13cd1e70169c51184ff718fdc7
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-29 02:49:40 +0000

    added recording buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import './App.css';
 
 function App() {
-  return <>Hello@!</>;
+  return (
+    <>
+      <h1 className=''>Say the vocabulary words</h1>
+      <div className="flex items-center justify-center">
+        <button>
+          <span className="square-symbol"></span>
+          <span>Stop</span>
+        </button>
+        <button>
+          <span className="circle-symbol"></span>
+          <span>Record</span>
+        </button>
+
+        <button>
+          <span className="triangle-symbol"></span>
+          <span>Review your recording</span>
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default App;
