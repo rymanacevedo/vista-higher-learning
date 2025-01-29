883f4f0e266aa4a85fb3e960104a920eec91a1e7
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-29 03:02:22 +0000

    added buttons and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import './App.css';
 
 function App() {
   return (
     <>
       <h1 className=''>Say the vocabulary words</h1>
-      <div className="flex items-center justify-center">
-        <button>
+      <div className="flex eitems-center justify-items-center">
+        <button className='inline-flex justify-center rounded-md border border-transparent cursor-pointer bg-blue-600 text-white'>
           <span className="square-symbol"></span>
           <span>Stop</span>
         </button>
-        <button>
+        <button className="inline-flex justify-center rounded-md border border-transparent cursor-pointer bg-red-600">
           <span className="circle-symbol"></span>
           <span>Record</span>
         </button>
 
-        <button>
+        <button className="inline-flex justify-center rounded-md border border-transparent cursor-pointer bg-green-800">
           <span className="triangle-symbol"></span>
           <span>Review your recording</span>
         </button>
